f39230e1f0fae8051f44bf6a72bd9ba41d899768
Olivia Huegel <49350811+oliviahuegel@users.noreply.github.com>
2020-05-03 23:41:47 -0400

    Add files via upload

---
+++ country.h
@@ -0,0 +1,30 @@
+/*
+Name: Olivia Huegel
+File Name: country.h
+Purpose: This file contains definitions and prototypes
+of functions that are defined in country.c.
+*/
+
+/* country.h */
+
+#ifndef COUNTRY_H
+#define COUNTRY_H
+
+#define NAME_LEN 25
+#define NUM_COUNTRIES 32
+
+struct country {
+  char name[NAME_LEN+1];
+  char group;
+  int goals;
+  int points;
+};
+
+void search_by_country(struct country countries[]);
+void search_by_group(struct country countries[]);
+void search_by_points(struct country countries[]);
+void print_sorted_by_goals(struct country countries[]);
+int  read_line(char str[], int n);
+void selection_sort(struct country countries[], int n);
+
+#endif
